6e7a101210f586cad307aeb4ff67f5e2518048be
Omar334-han <ofarhan720@gmail.com>
2024-07-12 10:53:27 +0600

    two page portfolio

---
+++ step01/js/main.js
@@ -0,0 +1,9 @@
+function showsidebar(){
+    const openbar = document.querySelector(".sidebar-container");
+    openbar.style.display = 'flex';
+}
+function offsidebar(){
+    const closebar = document.querySelector(".sidebar-container");
+    closebar.style.display = 'none';
+
+}
